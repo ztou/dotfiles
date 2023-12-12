e82d15e6bf5a3de6cca6528de120c5ec0fe87296
Johnny Huang <Johnny.Huang@autodesk.com>
2023-12-12 09:54:45 +0800

    Update alias for pyenv

--- .pyenv.bash
+++ .pyenv.bash
@@ -3,22 +3,22 @@ command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-function pv() {
+function vv() {
     venv_name="${1:-$(basename "$PWD")}"
     cmd="pyenv virtualenv $venv_name"
     echo running command: $cmd ...
     eval $cmd
 }
-function pa() {
+function va() {
     export PYENV_VIRTUALENV_DISABLE_PROMPT=1
     venv_name="${1:-$(basename "$PWD")}"
     cmd="pyenv activate $venv_name"
     echo running command: $cmd ...
     eval $cmd
 }
-alias pva='pv && pa'
-alias pd='pyenv deactivate'
-alias pvl='pyenv virtualenvs'
-alias pvd='pyenv virtualenv-delete'
-alias mkenv=pv
-alias workon=pa
+alias vva='vv && va'
+alias vd='pyenv deactivate'
+alias vvl='pyenv virtualenvs'
+alias vvd='pyenv virtualenv-delete -f'
+alias mkenv=vv
+alias workon=va
